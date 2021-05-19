ead3bf06fd4e0cb80f2843e8ad0db48389f50578
JOHN PAUL NAIJU <76506560+JOHNPAULNAIJU@users.noreply.github.com>
2021-05-19 19:54:40 +0530

    Update firebase.jsx

--- todo-mon/src/firebase.jsx
+++ todo-mon/src/firebase.jsx
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDY2j1LiccrwIYnQjcMZU_gctrf0EP2qxE",
+    apiKey: "",
     authDomain: "todo-mon.firebaseapp.com",
-    projectId: "todo-mon",
-    storageBucket: "todo-mon.appspot.com",
-    messagingSenderId: "159483898221",
-    appId: "1:159483898221:web:b7acd7a8b332caccd2d264",
-    measurementId: "G-HX8R5XHF5Q"
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
